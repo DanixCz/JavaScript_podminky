31bdd7ff98500c5f643e5875ef89e64d5c0c6a49
Daniel Zeman <Daniel.Zeman@student.souepl.cz>
2024-11-15 11:36:34 +0100

    1

---
+++ zacharn_te_mně.js
@@ -0,0 +1,22 @@
+function Control(){
+    const suffering = document.getElementById("paswd");
+    const eternal_suffering = document.getElementById("ctrl_paswd");
+    const output = document.getElementById("output")
+    const nick = document.getElementById("nick")
+    const code = document.getElementById("code")
+
+
+    //console.log(suffering);
+
+    if ((suffering.value == eternal_suffering.value) && (!isNaN(code.value))){
+        if ((nick.value != "") && (suffering.value != "") && (eternal_suffering.value != ""))
+        console.log("login is true");
+        output.innerHTML =("login is true");
+        output.style.color= "green" //inspirace od Šimona
+    }
+    else{
+        console.log("login is flase");
+        output.innerHTML =("login is false");
+        output.style.color= "red"
+    }
+}
